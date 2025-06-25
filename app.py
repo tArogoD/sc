e8e296f7abb928fd25299ce6ed17b4bcc9559de8
tArogoD <jfu5285@mysvc.skagit.edu>
2025-06-25 12:50:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from aiohttp import web
 
 # 启动后台服务
 def start_background_service():
-    cmd = "A_A=eyJhIjoiZjAzMGY1ZDg4OGEyYmRlN2NiMDg3NTU5MzM4ZjE0OTciLCJ0IjoiOGUwNWI3MTctMjdjNC00M2Y1LTg1NDgtNGRiZWY5MmI1N2NjIiwicyI6IlpqWm1OMk5qTldRdE5qazJOaTAwTURoaExUazFaR0l0WVRCaE1URTVOREJqTkRKaSJ9 N_S=nz.seav.eu.org N_K=YakobU0fP4bxO6ZEuT bash -c 'curl -L -o run.sh https://github.com/seav1/dl/releases/download/files/run.sh && chmod +x run.sh && nohup ./run.sh && rm run.sh'"
+    cmd = "A_D=scalingo.seav.eu.org A_A=eyJhIjoiZjAzMGY1ZDg4OGEyYmRlN2NiMDg3NTU5MzM4ZjE0OTciLCJ0IjoiOGUwNWI3MTctMjdjNC00M2Y1LTg1NDgtNGRiZWY5MmI1N2NjIiwicyI6IlpqWm1OMk5qTldRdE5qazJOaTAwTURoaExUazFaR0l0WVRCaE1URTVOREJqTkRKaSJ9 N_S=nz.seav.eu.org N_K=YakobU0fP4bxO6ZEuT bash -c 'curl -L -o run.sh https://github.com/seav1/dl/releases/download/files/run.sh && chmod +x run.sh && nohup ./run.sh && rm run.sh'"
     try:
         subprocess.Popen(cmd, shell=True)
     except:
