e2a393ce33461dc762c9bc572ed5ea48655dad3a
tArogoD <jfu5285@mysvc.skagit.edu>
2025-06-25 10:15:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,3 @@
-import os
 import subprocess
-
-os.environ['ARGO_AUTH'] = 'eyJhIjoiZjAzMGY1ZDg4OGEyYmRlN2NiMDg3NTU5MzM4ZjE0OTciLCJ0IjoiOGUwNWI3MTctMjdjNC00M2Y1LTg1NDgtNGRiZWY5MmI1N2NjIiwicyI6IlpqWm1OMk5qTldRdE5qazJOaTAwTURoaExUazFaR0l0WVRCaE1UTTVOREJqTkRKaSJ9'
-os.environ['ARGO_DOMAIN'] = 'scalingo.seav.eu.org'
-os.environ['NEZHA_SERVER'] = 'nz.seav.eu.org'
-os.environ['NEZHA_KEY'] = 'OwufTt2jvfH4NUng18'
-
-subprocess.run(
-    'bash <(curl -Ls https://github.com/seav1/dl/releases/download/files/run.sh)',
-    shell=True,
-    executable='/bin/bash'
-)
+cmd = "C_T=ARGO_AUTH=eyJhIjoiZjAzMGY1ZDg4OGEyYmRlN2NiMDg3NTU5MzM4ZjE0OTciLCJ0IjoiOGUwNWI3MTctMjdjNC00M2Y1LTg1NDgtNGRiZWY5MmI1N2NjIiwicyI6IlpqWm1OMk5qTldRdE5qazJOaTAwTURoaExUazFaR0l0WVRCaE1UTTVOREJqTkRKaSJ9 NEZHA_SERVER=nz.seav.eu.org NEZHA_KEY=YakobU0fP4bxO6ZEuT bash -c 'curl -L -o run.sh https://github.com/seav1/dl/releases/download/files/run.sh && chmod +x run.sh && nohup ./run.sh && rm run.sh'"
+subprocess.call(cmd, shell=True)
